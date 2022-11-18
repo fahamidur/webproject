4d520c9a0d3eb16c58b3805c573d6ba88708437f
Md. Fahamidur Rahaman Rafi <44564445+fahamidur@users.noreply.github.com>
2022-11-18 17:14:39 +0600

    First commit

---
+++ Covid 19 Test project/Covid19-TMS-Project/Covid19-TMS/SQL File/covidtmsdb.sql
@@ -0,0 +1,147 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(11) NOT NULL,
+  `AdminName` varchar(120) DEFAULT NULL,
+  `AdminuserName` varchar(20) NOT NULL,
+  `MobileNumber` int(12) NOT NULL,
+  `Email` varchar(120) NOT NULL,
+  `Password` varchar(120) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
+-- Table structure for table `tblpatients`
+--
+
+CREATE TABLE `tblpatients` (
+  `id` int(11) NOT NULL,
+  `FullName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(16) DEFAULT NULL,
+  `DateOfBirth` date DEFAULT NULL,
+  `GovtIssuedId` varchar(150) DEFAULT NULL,
+  `GovtIssuedIdNo` varchar(150) DEFAULT NULL,
+  `FullAddress` varchar(255) DEFAULT NULL,
+  `State` varchar(200) DEFAULT NULL,
+  `RegistrationDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `tblphlebotomist`
+--
+
+CREATE TABLE `tblphlebotomist` (
+  `id` int(11) NOT NULL,
+  `EmpID` varchar(100) DEFAULT NULL,
+  `FullName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(12) DEFAULT NULL,
+  `RegDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `tblreporttracking`
+--
+
+CREATE TABLE `tblreporttracking` (
+  `id` int(11) NOT NULL,
+  `OrderNumber` bigint(40) DEFAULT NULL,
+  `Remark` varchar(255) DEFAULT NULL,
+  `Status` varchar(120) DEFAULT NULL,
+  `PostingTime` timestamp NULL DEFAULT current_timestamp(),
+  `RemarkBy` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `tbltestrecord`
+--
+
+CREATE TABLE `tbltestrecord` (
+  `id` int(11) NOT NULL,
+  `OrderNumber` bigint(14) DEFAULT NULL,
+  `PatientMobileNumber` bigint(16) DEFAULT NULL,
+  `TestType` varchar(100) DEFAULT NULL,
+  `TestTimeSlot` varchar(120) DEFAULT NULL,
+  `ReportStatus` varchar(100) DEFAULT NULL,
+  `FinalReport` varchar(150) DEFAULT NULL,
+  `ReportUploadTime` varchar(200) DEFAULT NULL,
+  `RegistrationDate` timestamp NULL DEFAULT current_timestamp(),
+  `AssignedtoEmpId` varchar(150) DEFAULT NULL,
+  `AssigntoName` varchar(180) DEFAULT NULL,
+  `AssignedTime` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblpatients`
+--
+ALTER TABLE `tblpatients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblphlebotomist`
+--
+ALTER TABLE `tblphlebotomist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblreporttracking`
+--
+ALTER TABLE `tblreporttracking`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbltestrecord`
+--
+ALTER TABLE `tbltestrecord`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblpatients`
+--
+ALTER TABLE `tblpatients`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tblphlebotomist`
+--
+ALTER TABLE `tblphlebotomist`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tblreporttracking`
+--
+ALTER TABLE `tblreporttracking`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `tbltestrecord`
+--
+ALTER TABLE `tbltestrecord`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
